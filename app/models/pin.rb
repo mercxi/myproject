a005c57a8dab0d39304fe26c8978e4b8434169ce
Matthew Mercaldi <mercxi@gmail.com>
2013-04-01 21:44:47 -0400

    Phase 3

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,9 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :image
+  has_attached_file :image, styles: { medium: "320x240>"}
+  validates_attachment :image, presence: true,
+  					content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gifs']},
+					size: { less_than: 5.megabytes }
   validates :description, presence: true
 
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Myproject::Application.routes.draw do
 
   devise_for :users
 
-  root :to => 'pages#home'
+  root :to => 'pins#index'
 
   get 'about' => 'pages#about'
  # get "pages/home"
